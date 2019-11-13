ababd9f09b1a844d0342dd043e89b5cd02011546
Jam <superwangjianmin@163.com>
2019-11-13 09:27:51 +0800

    创建版本 1.0.0.

---
+++ NewTestCode/NewTestCode/Classses/JamTestCode.swift
@@ -0,0 +1,21 @@
+//
+//  JamTestPodView.swift
+//  UnmannedVehicle
+//
+//  Created by Jam 王 on 2019/11/12.
+//  Copyright © 2019 Jam 王. All rights reserved.
+//
+
+import UIKit
+
+class JamTestPodView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NewTestCode/NewTestCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTestCode
+//
+//  Created by Jam 王 on 2019/11/13.
+//  Copyright © 2019 Jam 王. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
